da357aed9c8ac2acd3b2a9e6a43cf5f715415030
Danish Syed <danishsyed2002@gmail.com>
2026-04-09 20:20:03 +0530

    add root

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from env.graders import grade_easy, grade_medium, grade_hard
+
+
+TASKS = [
+    {
+        "name": "easy",
+        "grader": grade_easy
+    },
+    {
+        "name": "medium",
+        "grader": grade_medium
+    },
+    {
+        "name": "hard",
+        "grader": grade_hard
+    }
+]
