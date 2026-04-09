ce84581fbd6b37713e50c131b8a277d6e32a9bee
Danish Syed <danishsyed2002@gmail.com>
2026-04-09 12:36:31 +0530

    fix task registry for validator

---
+++ env/registry.py
@@ -0,0 +1,17 @@
+from env.graders import grade_easy, grade_medium, grade_hard
+
+
+TASKS = [
+    {
+        "id": "easy",
+        "grader": grade_easy
+    },
+    {
+        "id": "medium",
+        "grader": grade_medium
+    },
+    {
+        "id": "hard",
+        "grader": grade_hard
+    }
+]
